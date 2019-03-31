bb335b25fce66d8a9ae922cc54ba898486b0f5e8
andreybannikov <andrewnnov@yandex.ru>
2019-03-31 05:42:06 +0300

    1.3. JavaDoc. Code convention[#125140]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,13 @@
 package ru.job4j.calculate;
 
+/**
+ * @author  Bannikov Andrey
+ */
 public class Calculate {
-
+    /**
+     * Main
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
