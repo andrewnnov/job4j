db7d71c1b76a26d1d808a98d1217eb83acfb2b00
andreybannikov <andrewnnov@yandex.ru>
2019-07-28 11:22:46 +0300

    5. Рефакторинг теста @Before @After[#125176]

--- chapter_002/src/test/java/ru/job4j/tracker/strategy/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/strategy/PaintTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker.strategy;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -8,17 +10,29 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class PaintTest {
+
+    // поле содержит дефолтный вывод в консоль.
+    private final PrintStream stdout = System.out;
+    // буфер для результата.
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream((this.out)));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
+
     @Test
     public void whenDrawSquare() {
-        //получаем ссылку на стандартный вывод в консоль
-        PrintStream stdout = System.out;
-        //создаем буфер для хранения вывода
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        //заменяем стандартный вывод на вывод в память для тестирования
-        System.setOut(new PrintStream(out));
-        //выполняем действия пишущие в консоль
+
         new Paint().draw(new Square());
-        //проверяем результат вычисления
         assertThat(new String(out.toByteArray()), is(new StringBuilder()
                 .append("++++")
                 .append("++++")
@@ -27,6 +41,22 @@ public class PaintTest {
                 .append(System.lineSeparator())
                 .toString()
         )
+        );
+        System.setOut(stdout);
+    }
+
+    @Test
+    public void whenDrawTriangle() {
+
+        new Paint().draw(new Triangle());
+        assertThat(new String(out.toByteArray()), is(new StringBuilder()
+                        .append("+")
+                        .append("++")
+                        .append("+++")
+                        .append("++++")
+                        .append(System.lineSeparator())
+                        .toString()
+                )
         );
         System.setOut(stdout);
     }
