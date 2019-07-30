cc8459eeb6f8d164b2e343d587458c0993fa340a
andreybannikov <andrewnnov@yandex.ru>
2019-07-30 06:55:30 +0300

    NEW 6. Доработать тесты в StartUITest проверяющие вывод данных в консоль.[#125177]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,6 +1,11 @@
 package ru.job4j.tracker;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
@@ -8,9 +13,25 @@ import static org.junit.Assert.assertThat;
 
 public class StartUITest {
 
+    Tracker tracker;
+    Input input;
+
+
+
+    @Before
+    public void createItem() {
+        tracker = new Tracker();
+    }
+
+    @After
+    public void todoInputEqualNull() {
+        this.input = null;
+    }
+
+
     @Test
     public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
-        Tracker tracker = new Tracker();
+        //Tracker tracker = new Tracker();
         Input input = new StubInput(new String[]{"0", "test1", "desk1", "6"});
         new StartUI(input, tracker).init();
         assertThat(tracker.getAll()[0].getName(), is("test1"));
@@ -18,7 +39,7 @@ public class StartUITest {
 
     @Test
     public void whenUpdateThenTrackerHasUpdateValue() {
-        Tracker tracker = new Tracker();
+        //Tracker tracker = new Tracker();
         Item item = new Item("test2", "desc2", 765L);
         tracker.add(item);
         Input input = new StubInput(new String[] {"2", item.getId(), "test99", "desc99", "6"});
@@ -28,7 +49,7 @@ public class StartUITest {
 
     @Test
     public void whenFindItemByIdTrackerHasThisItem() {
-        Tracker tracker = new Tracker();
+        //Tracker tracker = new Tracker();
         Item item = new Item("test2", "desc2", 765L);
         tracker.add(item);
         String itemId = tracker.getAll()[0].getId();
@@ -39,7 +60,7 @@ public class StartUITest {
 
     @Test
     public void whenFindItemByIdTrackerHasNotThisItem() {
-        Tracker tracker = new Tracker();
+        //Tracker tracker = new Tracker();
         Item item = new Item("test2", "desc2", 765L);
         tracker.add(item);
         String idNotInTracker = "1111222";
@@ -50,7 +71,7 @@ public class StartUITest {
 
     @Test
     public void whenFindItemByNameTrackerHasNotThisItem() {
-        Tracker tracker = new Tracker();
+        //Tracker tracker = new Tracker();
         Item item = new Item("test2", "desc2", 765L);
         Item item1 = new Item("test2", "desc3", 765L);
         tracker.add(item);
@@ -65,7 +86,7 @@ public class StartUITest {
 
     @Test
     public void whenDeleteItemByIdTrackerHasNotThisItem() {
-        Tracker tracker = new Tracker();
+        //Tracker tracker = new Tracker();
         Item item = new Item("test2", "desc2", 765L);
         Item item1 = new Item("test3", "desc3", 765L);
         tracker.add(item);
@@ -79,7 +100,7 @@ public class StartUITest {
 
     @Test
     public void whenShowAllItemsTrackerShowAllItem() {
-        Tracker tracker = new Tracker();
+        //Tracker tracker = new Tracker();
         Item item = new Item("test2", "desc2", 765L);
         Item item1 = new Item("test3", "desc3", 765L);
         tracker.add(item);
@@ -89,6 +110,20 @@ public class StartUITest {
         new StartUI(input, tracker).init();
         Item[] resultItem = tracker.getAll();
         assertThat(resultItem, is(expectedItem));
+    }
+
+    @Test
+    public void whenUserChooseShowAllItem() {
+        PrintStream stdout = System.out;
+        //Создаем буфер для хранения вывода
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        //заменяем стандартный вывод на вывод в память
+        System.setOut(new PrintStream(out));
+        //Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name1", "desc1"));
+        Input input = new StubInput(new String[]{"1", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(true, is(out.toString().contains("id заявки - " + item.getId())));
     }
 
 }
